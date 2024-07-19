055776019fff0aa907f894d309fd83119d460538
LuisChavezz <ing.luischavezz@gmail.com>
2024-07-18 22:34:33 -0600

    Initial structure & routes

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="p-24">
+    <div className="p-24">
       <h1>Hola mundo</h1>
       <h1 className={ `${titleFont.className} font-bold` }>Hola mundo</h1>
-    </main>
+    </div>
   );
 }
