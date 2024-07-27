bae8ef0dc3be669949a2ece163fad974fd7662db
LuisChavezz <ing.luischavezz@gmail.com>
2024-07-26 23:43:08 -0600

    Product details page & Size selector design

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,9 +1,53 @@
+import { SizeSelector } from "@/components";
+import { titleFont } from "@/config/fonts";
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: {
+    slug: string;
+  };
+}
 
 
-export default function ProductPage() {
+export default function ProductPage({ params }: Props) {
+
+  const slug = params.slug;
+  const product = initialData.products.find((product) => product.slug === slug);
+
+  if (!product) {
+    notFound();
+  }
+
   return (
-    <div>
-      <h1>Product Page</h1>
+    <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
+      {/* Slideshow */}
+      <div className="col-span-1 md:col-span-2">
+        Hola mundo.
+      </div>
+
+      {/* Details */}
+      <div className="col-span-1 px-5">
+        <h1 className={`${ titleFont.className } antialiased font-bold text-xl `}>
+          { product.title }
+        </h1>
+        <p className="text-lg mb-5">${ product.price }</p>
+
+        {/* Size selector */}
+        <SizeSelector availableSizes={ product.sizes } selectedSize={ "XL" } />
+
+        {/* Quantity selector */}
+
+
+        {/* Add to cart button */}
+        <div className="btn-primary my-5">
+          Add to cart
+        </div>
+
+        {/* Description */}
+        <h3 className="font-bold text-sm">Description</h3>
+        <p className="font-light">{ product.description }</p>
+      </div>
     </div>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,9 @@ export * from './ui/top-menu/TopMenu'
 export * from './ui/not-found/NotFound'
 export * from './ui/title/Title'
 
+// Product
+export * from './product/size-selector/SizeSelector'
+
 // Products
 export * from './products/product-grid/ProductGrid'
 export * from './products/product-grid/ProductGridItem'
