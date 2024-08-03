7240a542332fb32d673b6f14edd9d74e3270f5e6
LuisChavezz <ing.luischavezz@gmail.com>
2024-08-03 00:54:00 -0600

    Login & New Account Pages designs

--- src/app/auth/new-account/page.tsx
+++ src/app/auth/new-account/page.tsx
@@ -1,18 +1,18 @@
 import { titleFont } from '@/config/fonts';
 import Link from 'next/link';
 
-export default function Login() {
+export default function NewAccount() {
   return (
     <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
 
-      <h1 className={ `${ titleFont.className } text-4xl mb-5` }>Ingresar</h1>
+      <h1 className={ `${ titleFont.className } text-4xl mb-5` }>Nueva cuenta</h1>
 
       <div className="flex flex-col">
 
-        <label htmlFor="email">Correo electrónico</label>
+        <label htmlFor="email">Nombre completo </label>
         <input
           className="px-5 py-2 border bg-gray-200 rounded mb-5"
-          type="email" />
+          type="text" />
 
 
         <label htmlFor="email">Contraseña</label>
@@ -23,7 +23,7 @@ export default function Login() {
         <button
           
           className="btn-primary">
-          Ingresar
+            Crear cuenta
         </button>
 
 
@@ -35,9 +35,9 @@ export default function Login() {
         </div>
 
         <Link
-          href="/auth/new-account" 
+          href="/auth/login" 
           className="btn-secondary text-center">
-          Crear una nueva cuenta
+            Ingresar
         </Link>
 
       </div>
