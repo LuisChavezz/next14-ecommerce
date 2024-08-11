c0632c9c43e6cf5a6d7f491817305c272e15eca0
LuisChavezz <ing.luischavezz@gmail.com>
2024-08-11 00:29:03 -0600

    No products, then, redirect}

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,5 +1,6 @@
 import { getPaginatedProductsWithImages } from "@/actions";
 import { ProductGrid, Title } from "@/components";
+import { redirect } from "next/navigation";
 
 
 interface Props {
@@ -14,6 +15,10 @@ export default async function Home({ searchParams }: Props) {
   const page = ( searchParams.page ) ? parseInt( searchParams.page ) : 1;
 
   const { products } = await getPaginatedProductsWithImages({ page });
+
+  if ( products.length === 0 ) {
+    redirect('/')
+  }
 
   return (
     <>
