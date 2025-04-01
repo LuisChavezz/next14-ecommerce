121d047a892d0b906865ff1bc21aa6e1b54fc321
LuisChavezz <ing.luischavezz@gmail.com>
2025-04-01 13:21:01 -0600

    zustand, address store

---
+++ src/store/address/address-store.ts
@@ -0,0 +1,45 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface State {
+  address: {
+    firstName: string;
+    lastName: string;
+    address: string;
+    address2?: string;
+    postalCode: string;
+    city: string;
+    country: string;
+    phone: string;
+  }
+
+  // Methods
+  setAddress: (address: State["address"]) => void;
+}
+
+export const useAddressStore = create<State>()(
+  persist(
+    (set, get) => ({
+      // Initial state
+      address: {
+        firstName: "",
+        lastName: "",
+        address: "",
+        address2: "",
+        postalCode: "",
+        city: "",
+        country: "",
+        phone: "",
+      },
+
+      // Methods
+      setAddress: (address) => set({ address }),
+
+
+    }),
+    { // Persist configuration
+      name: 'address-storage',
+    }
+  )
+
+)
