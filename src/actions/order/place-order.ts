bb5be0f6b088d707dc488b60441c706281c044c9
LuisChavezz <ing.luischavezz@gmail.com>
2025-04-04 14:50:45 -0600

    order server action, step 2: quantity of products

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -2,6 +2,7 @@
 
 import { auth } from "@/auth.config";
 import type { Address, Size } from "@/interfaces";
+import prisma from "@/lib/prisma";
 
 
 interface ProductToOrder {
@@ -12,6 +13,7 @@ interface ProductToOrder {
 
 export const placeOrder = async( productIds: ProductToOrder[], address: Address ) => {
 
+  // Validate if user is authenticated
   const session = await auth()
   const userId = session?.user.id
   if ( !userId ) {
@@ -21,8 +23,18 @@ export const placeOrder = async( productIds: ProductToOrder[], address: Address 
     }
   }
 
-  console.log({ productIds, address });
+  // Get the product information
+  const products = await prisma.product.findMany({
+    where: {
+      id: {
+        in: productIds.map( product => product.productId )
+      }
+    },
+  })
 
+  // Calculate the quantity of products
+  const itemsInOrder = productIds.reduce(( count, product ) => count + product.quantity, 0)
 
+  // Calculate the subtotal, total and taxes
 
 }
