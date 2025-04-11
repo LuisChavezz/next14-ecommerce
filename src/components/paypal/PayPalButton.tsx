88383399953da1e7795ec5d5cdde931cd64c3cfb
LuisChavezz <ing.luischavezz@gmail.com>
2025-04-11 15:17:27 -0600

    PayPal buttons

--- src/components/paypal/PayPalButton.tsx
+++ src/components/paypal/PayPalButton.tsx
@@ -1,9 +1,21 @@
 'use client'
 
-import { PayPalButtons } from "@paypal/react-paypal-js"
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js"
 
 
 export const PayPalButton = () => {
+
+  const [{ isPending }] = usePayPalScriptReducer()
+
+  if ( isPending ) {
+    return (
+      <div className="animate-pulse space-y-4">
+        <div className="h-11 bg-gray-300 rounded" />
+        <div className="h-11 bg-gray-300 rounded" />
+      </div>
+    )
+  }
+
   return (
     <PayPalButtons />
   )
