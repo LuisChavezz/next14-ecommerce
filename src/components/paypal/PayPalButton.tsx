74a9ca86a9bc0d716bf65db4783896dd1237d6e8
LuisChavezz <ing.luischavezz@gmail.com>
2025-04-13 14:23:27 -0600

    PayPal, create payment order

--- src/components/paypal/PayPalButton.tsx
+++ src/components/paypal/PayPalButton.tsx
@@ -1,22 +1,50 @@
 'use client'
 
-import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js"
+import { PayPalButtons, PayPalButtonsComponentProps, usePayPalScriptReducer } from "@paypal/react-paypal-js"
+import { CreateOrderActions, CreateOrderData } from "@paypal/paypal-js"
 
 
-export const PayPalButton = () => {
+interface Props {
+  orderId: string
+  amount: number
+}
+
+export const PayPalButton = ({ orderId, amount }: Props) => {
 
   const [{ isPending }] = usePayPalScriptReducer()
+
+  const rountedAmount = ( Math.round( amount * 100 ) / 100 ).toFixed( 2 )
 
   if ( isPending ) {
     return (
       <div className="animate-pulse space-y-4">
-        <div className="h-11 bg-gray-300 rounded" />
+        <div className="h-11 bg-gray-300 rounded" />  
         <div className="h-11 bg-gray-300 rounded" />
       </div>
     )
   }
 
+  const createOrder: PayPalButtonsComponentProps["createOrder"] = async (data, actions): Promise<string> => {
+
+    const transactionId = await actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            currency_code: 'USD',
+            value: rountedAmount
+          }
+        }
+      ],
+      intent: 'CAPTURE',
+    })
+
+    console.log({ transactionId })
+    return transactionId
+  }
+
   return (
-    <PayPalButtons />
+    <PayPalButtons 
+      createOrder={  createOrder }
+    />
   )
 }
